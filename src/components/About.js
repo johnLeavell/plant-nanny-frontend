6aee94fa94c82071849678b574455928bddd7d14
John Leavell <johnleavell@gmail.com>
2020-12-07 07:07:08 -0600

    added routing and about page, will style next

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h4>This is the about page.. George jsut screamed out from his sleep... something about a racoon..</h4>
+        </div>
+    )
+}
+
+export default About;
