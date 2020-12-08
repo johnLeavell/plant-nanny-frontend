efe2e2d0a0668bba839a949ba20acd55fcb350be
John Leavell <johnleavell@gmail.com>
2020-12-08 06:58:16 -0600

    added shell components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
+import './style.css';
+import { BrowserRouter as Router,  Route, NavLink } from 'react-router-dom';
 import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import { CSSTransition } from 'react-transition-group'
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+const routes = [
+  { path: '/', name: 'Home', Component: Home },
+  { path: '/about', name: 'About', Component: About },
+  { path: '/contact', name: 'Contact', Component: Contact },
+]
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <header className="App-header">
-      <Switch>
-        <Route path='/about' render={(props) => <About {...props} /> }/>
-      </Switch>
-
-      </header>
-    </div>
+      <>
+        <Navbar bg="dark" expand='lg'>
+          <Nav className="mx-auto">
+            {routes.map(route => (
+              <Nav.Link
+                key={route.path}
+                as={NavLink}
+                to={route.path}
+                activeClassName="active"
+                exact
+              >
+                {route.name}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar>
+        <Container className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </Container>
+      </>
     </Router>
-  );
+  )
 }
 
 export default App;
